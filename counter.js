0ce0e66e0af44fcb9d7cd4a22985943c42246c76
wjl8026 <wjl8026@zjgsu.edu.cn>
2017-11-19 20:33:25 +0800

    初次增加文件到版本库

---
+++ counter.js
@@ -0,0 +1,16 @@
+
+var c=0
+var t
+function timedCount()
+{
+document.getElementById('txt').value=c
+c=c+1
+t=setTimeout("timedCount()",1000)
+}
+
+function stopCount()
+{
+c=0;
+setTimeout("document.getElementById('txt').value=0",0);
+clearTimeout(t);
+}
